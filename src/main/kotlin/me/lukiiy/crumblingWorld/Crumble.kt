61bd35c27599b6c0daeb638deccf116d3ea0ebbb
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2024-10-12 22:04:09 -0300

    Initial commit

---
+++ src/main/kotlin/me/lukiiy/crumblingWorld/Crumble.kt
@@ -0,0 +1,53 @@
+package me.lukiiy.crumblingWorld
+
+import org.bukkit.Bukkit
+import org.bukkit.GameMode
+import org.bukkit.block.Block
+import org.bukkit.block.BlockFace
+import org.bukkit.event.Listener
+import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.scheduler.BukkitRunnable
+import org.bukkit.scheduler.BukkitTask
+
+class Crumble : JavaPlugin(), Listener {
+    val radius = 4 // half
+    private var runnable: BukkitTask? = null
+    private val updateTime = 1
+
+    override fun onEnable() {
+        runnable = object : BukkitRunnable() {
+            override fun run() {
+                for (player in Bukkit.getOnlinePlayers()) {
+                    if (player.gameMode == GameMode.CREATIVE ||
+                        player.gameMode == GameMode.SPECTATOR ||
+                        player.isDead) continue
+
+                    val loc = player.location
+                    val (px, py, pz) = Triple(loc.blockX, loc.blockY, loc.blockZ)
+
+                    for (x in (px - radius)..(px + radius)) {
+                        for (y in (py - radius)..(py + radius)) {
+                            for (z in (pz - radius)..(pz + radius)) {
+                                val block = player.world.getBlockAt(x, y, z)
+                                if (block.isEmpty ||
+                                    block.isLiquid ||
+                                    !block.getRelative(BlockFace.DOWN).isEmpty ||
+                                    block.type.blastResistance > 100) continue
+
+                                transform(block)
+                            }
+                        }
+                    }
+                }
+            }
+        }.runTaskTimer(this, 0L, updateTime * 20L)
+    }
+
+    override fun onDisable() {runnable!!.cancel()} // 2NotWorry
+
+    fun transform(block: Block) {
+        val fall = block.world.spawnFallingBlock(block.location, block.blockData)
+        fall.dropItem = false
+        fall.setHurtEntities(true)
+    }
+}
