af9825a28dfd955730abe9644a46234811e4cda1
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2024-12-18 20:06:02 -0300

    patch

--- src/main/kotlin/me/lukiiy/crumblingWorld/Crumble.kt
+++ src/main/kotlin/me/lukiiy/crumblingWorld/Crumble.kt
@@ -2,6 +2,7 @@ package me.lukiiy.crumblingWorld
 
 import org.bukkit.Bukkit
 import org.bukkit.GameMode
+import org.bukkit.Material
 import org.bukkit.block.Block
 import org.bukkit.block.BlockFace
 import org.bukkit.event.Listener
@@ -10,7 +11,7 @@ import org.bukkit.scheduler.BukkitRunnable
 import org.bukkit.scheduler.BukkitTask
 
 class Crumble : JavaPlugin(), Listener {
-    val radius = 4 // half
+    val radius = 5 // x2
     private var runnable: BukkitTask? = null
     private val updateTime = 1
 
@@ -35,6 +36,7 @@ class Crumble : JavaPlugin(), Listener {
                                     block.type.blastResistance > 100) continue
 
                                 transform(block)
+                                block.type = Material.AIR
                             }
                         }
                     }
@@ -46,7 +48,7 @@ class Crumble : JavaPlugin(), Listener {
     override fun onDisable() {runnable!!.cancel()} // 2NotWorry
 
     fun transform(block: Block) {
-        val fall = block.world.spawnFallingBlock(block.location, block.blockData)
+        val fall = block.world.spawnFallingBlock(block.location.add(.5, 0.0, .5), block.blockData)
         fall.dropItem = false
         fall.setHurtEntities(true)
     }
