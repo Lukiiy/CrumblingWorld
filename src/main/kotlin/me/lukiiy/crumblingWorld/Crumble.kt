39b2ed94900b11b67b3ccc9cdd072b268cbfc899
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2024-12-20 00:33:55 -0300

    Update Crumble.kt

--- src/main/kotlin/me/lukiiy/crumblingWorld/Crumble.kt
+++ src/main/kotlin/me/lukiiy/crumblingWorld/Crumble.kt
@@ -12,36 +12,35 @@ import org.bukkit.scheduler.BukkitTask
 
 class Crumble : JavaPlugin(), Listener {
     val radius = 5 // x2
-    private var runnable: BukkitTask? = null
-    private val updateTime = 1
+    private val updateTime = 1 // in Seconds
 
     override fun onEnable() {
-        runnable = object : BukkitRunnable() {
-            override fun run() {
-                Bukkit.getOnlinePlayers().filter {
-                    it.gameMode in listOf(GameMode.SURVIVAL, GameMode.ADVENTURE) && !it.isDead
-                }.forEach {
-                    val (px, py, pz) = with(it.location) { Triple(blockX, blockY, blockZ) }
+        Bukkit.getScheduler().runTaskTimer(this, Runnable {
+            Bukkit.getOnlinePlayers().filter {
+                it.gameMode in listOf(GameMode.SURVIVAL, GameMode.ADVENTURE) && !it.isDead
+            }.forEach {
+                val (px, py, pz) = with(it.location) { Triple(blockX, blockY, blockZ) }
 
-                    (px - radius..px + radius).forEach { x ->
-                        (py - radius..py + radius).forEach { y ->
-                            (pz - radius..pz + radius).forEach { z ->
-                                it.world.getBlockAt(x, y, z).takeIf { block ->
-                                    block.isEmpty && !block.isLiquid && block.getRelative(BlockFace.DOWN).isEmpty && block.type.blastResistance <= 100
-                                }?.apply { transform(this) }
+                (px - radius..px + radius).forEach { x ->
+                    (py - radius..py + radius).forEach { y ->
+                        (pz - radius..pz + radius).forEach { z ->
+                            it.world.getBlockAt(x, y, z).takeIf { block ->
+                                !block.isEmpty && !block.isLiquid && block.getRelative(BlockFace.DOWN).isEmpty && block.type.blastResistance <= 100
+                            }?.let { block ->
+                                transform(block)
+                                block.type = Material.AIR
                             }
                         }
                     }
                 }
             }
-        }.runTaskTimer(this, 0L, updateTime * 20L)
+        }, 0L, 20L * updateTime)
     }
 
-    fun transform(block: Block) {
+    private fun transform(block: Block) {
         block.world.spawnFallingBlock(block.location.add(0.5, 0.0, 0.5), block.blockData).apply {
             dropItem = false
             setHurtEntities(true)
         }
-        block.type = Material.AIR
     }
 }
